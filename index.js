a7f4b08170c227d9b92ba40c1c5aa2eee84f4283
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-07-17 16:55:41 +0300

    This commit adds the input of the daily outputs to the page. The inputs can take data and give an output.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-document.getElementById("title").innerHTML = "Farmville";
-document.getElementById("foot").innerHTML = "Farmville";
+// document.getElementById("title").innerHTML = "Farmville";
+// document.getElementById("foot").innerHTML = "Farmville";
+
+
+function findTotal(){
+    var arr = document.getElementsByName('shed');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = tot;
+}
+
+
