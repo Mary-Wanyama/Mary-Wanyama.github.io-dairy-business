a6b3de2b15b2c39a95d88f9ee368b394e5f24330
Mary <immaculatemariah@gmail.com>
2023-05-22 04:54:05 +0300

    javascript total

--- index.js
+++ index.js
@@ -8,51 +8,6 @@ for (let i = 0; i < arr.length; i++) {
     nums.push(parseInt(this.value))
     const sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     console.log(sum)
-    document.querySelector('total').textContent = sum
+    document.querySelector('.total').textContent = sum
   })
 }
-function Listener() {
-  if(parseInt(arr[i].value)){
-    
-    document.querySelector(".total").textContent = 0 + parseInt(arr[i].value)
-    document.querySelector('.week').textContent = parseInt( document.querySelector(".total").textContent) * 7
-
-    var thirtyone = document.querySelectorAll(".jan")
-
-    for (let i = 0; i < thirtyone.length; i++) {
-      document.querySelectorAll(".jan")[i].textContent = parseInt( document.querySelector(".total").textContent)* 31
-    }
-    var thirty = document.querySelectorAll('.ap')
-    for (let i = 0; i < thirty.length; i++) {
-      document.querySelectorAll(".ap")[i].textContent = parseInt( document.querySelector(".total").textContent)* 30
-    }
-    document.querySelector('.feb').textContent =  parseInt( document.querySelector(".total").textContent)* 28
-
-
-    document.querySelector('.year').textContent =  parseInt( document.querySelector(".total").textContent)* 365
-  } 
-
-}
-
-// function add(key) {
-//   switch (key) {
-//     case 'shed1':
-      
-//       break;
-//       case value:
-      
-//       break;
-//       case value:
-      
-//       break;
-//       case value:
-      
-//       break;
-//       case value:
-      
-//       break;
-  
-//     default:
-//       break;
-//   }
-// }
