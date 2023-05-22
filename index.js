1f15c10f54310423fd5673fef38c827d3407d4ed
Mary <immaculatemariah@gmail.com>
2023-05-22 04:45:25 +0300

    total

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 document.getElementById("title").innerHTML = "Farmville";
 var arr = document.querySelectorAll("input")
+var nums = []
+
 for (let i = 0; i < arr.length; i++) {
   document.querySelectorAll("input")[i].addEventListener("blur", function(){
     console.log(this.value)
+    nums.push(this.value)
+    const sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    console.log(sum)
   })
 }
 function Listener() {
