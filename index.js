3b1611b8dc2c9527fab78d7e0ad31857e8bb86a8
Mary <immaculatemariah@gmail.com>
2023-05-22 09:11:35 +0300

    numbers

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var nums = []
 for (let i = 0; i < arr.length; i++) {
   document.querySelectorAll("input")[i].addEventListener("blur", function(){
     console.log(document.querySelectorAll("input")[i].value)
-    nums.length<=5
-    nums.push(parseInt(document.querySelectorAll("input")[i].value))
+
+    nums = [...parseInt(document.querySelectorAll("input")[i].value)]
+
+
     const sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     console.log(sum)
     document.querySelector('.total').textContent = sum
