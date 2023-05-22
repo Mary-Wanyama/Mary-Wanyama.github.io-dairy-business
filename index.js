2f7b29f367f98663d0eacaae5724bf661550aae0
Mary <immaculatemariah@gmail.com>
2023-05-22 04:47:39 +0300

    total

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var nums = []
 for (let i = 0; i < arr.length; i++) {
   document.querySelectorAll("input")[i].addEventListener("blur", function(){
     console.log(this.value)
-    nums.push(this.value)
+    nums.push(parseInt(this.value))
     const sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     console.log(sum)
   })
