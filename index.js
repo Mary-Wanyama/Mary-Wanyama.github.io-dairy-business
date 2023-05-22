435bfa74b73eec0cb785a7cdf026044eb7607530
Mary <immaculatemariah@gmail.com>
2023-05-22 04:28:10 +0300

    functional

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.getElementById("title").innerHTML = "Farmville";
 var arr = document.querySelectorAll("input")
 for (let i = 0; i < arr.length; i++) {
-  document.querySelectorAll("input")[i].addEventListener("blur", Listener)
+  document.querySelectorAll("input")[i].addEventListener("blur", function(){
+    alert("hello there")
+  })
 }
 function Listener() {
   if(parseInt(arr[i].value)){
